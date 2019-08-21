fc40070858d31283b9d22724bbad83efa1914923
lxb <techsayer@163.com>
2019-08-21 15:50:11 +0800

    fix bug:
    1. a general 'a' element is used for menu link, change to 'route link'
    that is a VueRoute component.

--- src/layout/components/Sidebar/Link.vue
+++ src/layout/components/Sidebar/Link.vue
@@ -1,48 +1,48 @@
 
 <template>
-  <!-- eslint-disable vue/require-component-is -->
-  <component v-bind="linkProps(to)">
-    <slot/>
-  </component>
+	<!-- eslint-disable vue/require-component-is -->
+	<component v-bind="linkProps(to)">
+		<slot/>
+	</component>
 </template>
 
 <script>
 //import { isExternal } from "@/utils/validate";
 
 export default {
-  props: {
-    to: {
-      type: String,
-      required: true
-	},
-	isExternal: {
-	  type: Boolean,
-	  required: false,
-	  default: false,
-	}
-  },
-  methods: {
-    linkProps(url) {
-		if (this.isExternal) {
-			return {
-				is: "a",
-				href: url,
-				target: "_blank",
-				rel: "noopener"
-			};
+	props: {
+		to: {
+			type: String,
+			required: true
+		},
+		isExternal: {
+			type: Boolean,
+			required: false,
+			default: false
 		}
+	},
+	methods: {
+		linkProps(url) {
+			if (this.isExternal) {
+				return {
+					is: "a",
+					href: url,
+					target: "_blank",
+					rel: "noopener"
+				};
+			}
+			/*
 		return {
 			is: "a",
 			href: url,
 			rel: "noopener"
 		};
-	  /*
-      return {
-        is: "router-link",
-        to: url
-	  };
-	  */
-    }
-  }
+		*/
+			return {
+				is: "router-link",
+				to: url
+			};
+		}
+	}
 };
 </script>
